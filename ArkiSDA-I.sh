e0d9c438dfb9bc680cf9f5c8b1fa8d52b63cecd0
isthian <67603674+isthian@users.noreply.github.com>
2022-09-25 10:15:13 -0500

    Update ArkiSDA-I.sh
    Signed-off-by: isthian <67603674+isthian@users.noreply.github.com>

--- ArkiSDA-I.sh
+++ ArkiSDA-I.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 export PATH="$HOME/.local/bin:$PATH"
-clear
-
-echo "AUTOMATIC1111 SD-WebUI Installation by Arkitecc#0339 @ https://ko-fi.com/arkitecc"
-# but, like, Joe helped.
-echo "IMPORTANT: If this is the first time you're running these scripts, each script will require you to enter 'chmod +x scriptname.sh && bash scriptname.sh' to give Linux the proper permissions to execute the scripts for the first time. Afterward, you can run them simply with 'bash scriptname.sh'"
-echo "Run the ArkiSDA-U.sh script to pull the latest changes from the AUTOMATIC1111 SD-WebUI repo." 
-echo "Run the ArkiSDA-R.sh (default mode) or ArkiSD-RT.sh (Textual Inversion Mode) scripts if you need to launch SD-WebUI in the future."
-echo "Run the ArkiSDA-RT.sh script afterward to launch SD with support for Textual Inversion embeddings."
 echo " "
-echo "CTRL+SHIFT+V to paste in direct download link to model.ckpt (I personally use Dropbox to host mine):"
+
+echo "First run enter 'chmod +x scriptname.sh && bash scriptname.sh' "
+echo "Run ArkiSDA-U.sh to pull latest AUTOMATIC1111 repo." 
+echo "Run ArkiSDA-R.sh (default mode) or ArkiSD-RT.sh (Textual Inversion Mode) to launch SD-WebUI."
+echo "Run ArkiSDA-RT.sh script after to launch SD with Textual Inversion embeddings."
+echo " "
+echo "paste direct download link to model.ckpt:"
 
 read CHECKPOINT
 
-clear
 echo "Updating Packages..."
 
 #Package Updating
@@ -21,28 +18,36 @@ sudo apt-get update
 sudo apt install python3-pip -y
 sudo apt-get upgrade -y
 
-
-# Miniconda Installation
-cd /tmp/
-curl -LO https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+# Anaconda Installation
+PIP_EXISTS_ACTION=i
+cd ~/tmp
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 bash Anaconda3-2022.05-Linux-x86_64.sh -b -u
+cd ~/anaconda3
+PIP_EXISTS_ACTION=w
+pip install opencv-python
+PIP_EXISTS_ACTION=i
+
 source ~/anaconda3/bin/activate 
 conda init bash
 cd
 
-
-
-clear
+echo " "
+echo " "
+echo " "
 echo "Installing AUTOMATIC1111 SD-WebUI..."
 
 # AUTOMATIC1111 Installation
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd /home/user/stable-diffusion-webui/
+mkdir models
+cd models
+wget $CHECKPOINT 
+cd ..
 conda env create -f environment-wsl2.yaml
 conda activate automatic
 mkdir embeddings
 mkdir repositories
-mkdir models
 git clone https://github.com/CompVis/stable-diffusion.git repositories/stable-diffusion
 git clone https://github.com/CompVis/taming-transformers.git repositories/taming-transformers
 git clone https://github.com/sczhou/CodeFormer.git repositories/CodeFormer
@@ -56,12 +61,16 @@ pip3 install -r requirements.txt
 
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth
 cd /home/user/stable-diffusion-webui/models/
+echo " "
+echo "Paste model.ckpt link if first tries failed"
 
-curl -LO $CHECKPOINT
+read LINXY
+
+curl -LO $LINXI
 
 cd /home/user/stable-diffusion-webui/
 
-clear
+echo " "
 echo "Installing Local Tunnel..."
 
 # Local Tunnel Installation
@@ -71,7 +80,7 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 sudo npm install -g localtunnel
-echo "Click on the link with the random name below to access AUTOMATIC1111's SD-WebUI on your local PC once the initialization finishes and shows you the localhost link."
+echo "Click link below to access SD-WebUI locally after setup finishes and displays localhost link."
 lt --port 7860 &
  	
 echo "Initializing AUTOMATIC1111's SD-WebUI..."
